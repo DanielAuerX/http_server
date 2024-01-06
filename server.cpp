91f59756d6f077ae168055f54eb10535c1021698
broccoli <lekker.broccoli@gmail.com>
2024-01-06 14:18:36 +0100

    stopping server smoothly

--- server.cpp
+++ server.cpp
@@ -1,9 +1,29 @@
 #include "http_tcpserver.h"
 
+#include <csignal>
 
-int main(){
+http::TcpServer* serverInstance = nullptr;
+
+void signalHandler(int signal)
+{
+    if (serverInstance)
+    {
+        delete serverInstance;
+        serverInstance = nullptr;
+    }
+    exit(0);
+}
+
+int main()
+{
     using namespace http;
+    
     TcpServer server = TcpServer("0.0.0.0", 8080);
+    serverInstance = &server;
+
+    signal(SIGINT, signalHandler);
+    
     server.startListen();
+    
     return 0;
 }
